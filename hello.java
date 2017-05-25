ce6e4eea6c2dcf3d8161352b46b2f7286965a792
afei <1147383031@qq.com>
2017-05-25 20:28:14 +0800

    motified hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class hello{
 	public static void main(String args[]){
-        System.out.println("hello git");
+        System.out.println("happy every");
+		System.out.println("happy myself");
 	}
 }
