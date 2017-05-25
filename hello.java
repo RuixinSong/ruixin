57f1ebb5eab1c23293b1d7017090cbafea93acf1
afei <1147383031@qq.com>
2017-05-25 22:14:40 +0800

    add hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello{
 	public static void main(String args[]){
         System.out.println("happy every");
 		System.out.println("happy myself");
+		System.out.println("happy kang");
 	}
 }
